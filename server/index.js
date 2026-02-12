8fe38a8b8848b01453eb0c1b94170c17397c7b5c
Shani <shani2405sk@gmail.com>
2026-02-12 15:05:16 +0530

    installation of express

--- server/index.js
+++ server/index.js
@@ -1,24 +1,50 @@
 const http = require('http');
 const fs = require('fs');
 const url = require('url');
+const express = require('express');
 
-const myServer = http.createServer((req, res)=> {
-    if(req.url === '/favicon.ico') return res.end();
-    const log = `${Date.now()}: ${req.url} New Request Received\n`;
-    const myurl = url.parse(req.url, true);
-    console.log(myurl);
-    fs.appendFile('log.txt',log, (err, data) =>{
-        switch (myurl.pathname) {
-            case '/' : res.end("Home Page"); 
-            break;
-            case '/about' :
-            const username = myurl.query.myname
-            res.end(`Hi ${username}`);
-            break;
-            default: res.end("404 not found")
-        }
-    });
-    
+const app = express();
+
+app.get('/', (req, res) => {
+    return res.send("Home from home page");
 });
 
-myServer.listen(8000, ()=> console.log("Server Started!"));
+app.get('/about', (req, res) => {  
+    return res.send("Hello from about page"+ " hey " + req.query.name);
+});
+
+app.listen(8000, () => console.log("Server Started!"));
+
+// const myServer = http.createServer(app);
+
+// const myServer = http.createServer((req, res)=> {
+//     if(req.url === '/favicon.ico') return res.end();
+//     const log = `${Date.now()}: ${req.method} ${req.url} New Request Received\n`;
+//     const myUrl = url.parse(req.url, true);
+//     console.log(myUrl);
+//     fs.appendFile('log.txt',log, (err, data) =>{
+//         switch (myUrl.pathname) {
+//             case '/' :
+//             if(req.method=== 'GET') res.end("Home Page"); 
+//             break;
+//             case '/about' :
+//             const username = myUrl.query.myname
+//             res.end(`Hi ${username}`);
+//             break;
+            
+//             case '/search':
+//                 const search = myUrl.query.search_query;
+//                 res.end(`Here are your search results for `+search);
+//                 break
+
+//             case '/signup' :
+//                 if(req.method === 'Get') res.end("This is a signup Form");
+//                 else if(req.method === 'POST') res.end("Success");
+//                 break;
+//             default: res.end("404 not found")
+//         }
+//     });
+    
+// });
+
+// myServer.listen(8000, ()=> console.log("Server Started!"));
